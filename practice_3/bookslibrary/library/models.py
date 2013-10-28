b49b55476a9a0813a2b8de5f8b169658c9ca6ee9
Taisia_Vostrikova <taika20@mail.ru>
2013-10-28 22:40:22 +0400

    Results Practice_3 Vostrikova Taisia

---
+++ practice_3/bookslibrary/library/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from datetime import datetime
+
+class Author(models.Model):
+    first_name = models.CharField('first_name',        max_length=30)
+    last_name = models.CharField('last_name', max_length=30)
+    email = models.EmailField(blank = True, null = True)
+
+    def __unicode__(self):
+        return '%s %s' % (self.last_name, self.first_name)
+
+    def get_absolute_url(self):
+        return '/library/authors/%d/' % (self.id)
+
+
+class Book(models.Model):
+    title = models.CharField('title',max_length=128)
+    authors = models.ManyToManyField('Author')
+    publisher = models.ForeignKey('Publisher')
+    publication_date = models.DateField('publication_date', default=datetime.now())
+
+    def __unicode__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return '/library/books/%d/' % (self.id)
+
+    def get_authors(self):
+       return ', '.join([author.first_name +' '+ author.last_name for author in self.authors.all()])
+
+
+class Publisher(models.Model):
+    title = models.CharField('title',max_length=32)
+    address = models.TextField('address')
+    city = models.CharField('city',max_length=64)
+    country = models.CharField('country',max_length=64)
+    website = models.URLField('website')
+
+    def __unicode__(self):
+        return '%s (%s)' % (self.title, self.website)
+# Create your models here.
---
+++ practice_3/bookslibrary/library/views.py
@@ -0,0 +1,23 @@
+# Create your views here.
+
+from library.models import Book
+from library.models import Author
+from library.models import Publisher
+from django.shortcuts import render
+
+def books(request, sub):
+    d = {'books':Book.objects.all()}
+    return render(request, 'books.html', d)
+
+def book(request, sub):
+    book = Book.objects.get(id=sub)
+    d = {'book':book, 'authors': book.authors.all()}
+    return render(request, 'book.html', d)
+
+def authors(request):
+    d = {'authors':Author.objects.all()}
+    return render(request, 'authors.html', d)
+
+def author(request, sub):
+    d = {'author':Author.objects.get(id=sub)}
+    return render(request, 'author.html', d)
